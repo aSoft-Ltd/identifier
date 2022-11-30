ee4d1fddff94840e306eb4d7eb1b09928e7a97e7
andylamax <andylamax@programmer.net>
2022-11-30 15:55:28 +0300

    Routine sync up

--- core/src/commonMain/kotlin/identifier/Phone.kt
+++ core/src/commonMain/kotlin/identifier/Phone.kt
@@ -7,15 +7,23 @@ import identifier.serializers.PhoneSerializer
 import kotlinx.serialization.Serializable
 import kotlin.js.JsExport
 import kotlin.js.JsName
+import kotlin.jvm.JvmName
+import kotlin.jvm.JvmStatic
 
 @Serializable(with = PhoneSerializer::class)
 interface Phone : Comm {
     companion object {
+        @JvmStatic
+        @JvmName("of")
         operator fun invoke(value: String): Phone = PhoneImpl(value)
 
+        @JvmStatic
+        @JvmName("of")
         @JsName("fromInt")
         operator fun invoke(value: Int): Phone = PhoneImpl(value.toString())
 
+        @JvmName("of")
+        @JvmStatic
         @JsName("_ignore_fromLong")
         operator fun invoke(value: Long): Phone = PhoneImpl(value.toString())
     }
