5b4a6ca19efd1b1094b5cfc8f5e182445ea67077
andylamax <andylamax@programmer.net>
2023-05-02 18:40:47 +0300

    Made authenticator compile

--- legal/sdk/core/src/commonMain/kotlin/identifier/IdentifierScopeConfig.kt
+++ legal/sdk/core/src/commonMain/kotlin/identifier/IdentifierScopeConfig.kt
@@ -1,7 +1,10 @@
 package identifier
 
 import keep.Cacheable
+import lexi.Logable
 
-interface IdentifierScopeConfig<out A> : Cacheable {
+interface IdentifierScopeConfig<out A> : Cacheable, Logable {
     val api: A
+
+    fun <R> map(transformer: (A) -> R): IdentifierScopeConfig<R>
 }
