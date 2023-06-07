cbc3c6afb6bfd0d9860dd5cc2c7b8d523301d481
andylamax <andylamax@programmer.net>
2023-06-07 10:14:45 +0300

    Added user information on a sale indenture form

--- legal/sdk/core/src/commonMain/kotlin/identifier/utils/CacheUtils.kt
+++ legal/sdk/core/src/commonMain/kotlin/identifier/utils/CacheUtils.kt
@@ -5,11 +5,16 @@ import keep.Cache
 import keep.loadOrNull
 import identifier.LegalEntity
 import koncurrent.Later
+import symphony.removeSelectedItem
 
 inline fun Cache.loadSelectedCustomer() = loadOrNull(CacheKeys.SELECTED_ITEM, LegalEntity.serializer())
 
+@Deprecated(
+    message = "do not use this",
+    replaceWith = ReplaceWith("removeSelectedItem()", "symphony.removeSelectedItem")
+)
 inline fun Cache.removeSelectedCustomer() = remove(CacheKeys.SELECTED_ITEM)
 
 internal fun Cache.removeCustomer(uid: String?) = loadSelectedCustomer().andThen {
-    if (uid == it?.uid) removeSelectedCustomer() else Later(null)
+    if (uid == it?.uid) removeSelectedItem() else Later(null)
 }
