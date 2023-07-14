be2207ab5608734e0af92dac3fb408f56de287e4
andylamax <andylamax@programmer.net>
2023-07-14 17:21:23 +0300

    Friday evening commit

--- legal/dtos/src/commonMain/kotlin/identifier/IdentifierSettings.kt
+++ legal/dtos/src/commonMain/kotlin/identifier/IdentifierSettings.kt
@@ -5,6 +5,8 @@ import geo.Country
 interface IdentifierSettings<out D> {
     val data: D
     val country: Country
+    val company: CorporateDto
+    val user: IndividualDto
 
     fun <R> map(transformer: (D) -> R): IdentifierSettings<R>
 }
