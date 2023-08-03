93e95a0b4d09e85f32fbc1f902e9c6ff086b0e2a
andylamax <andylamax@programmer.net>
2023-08-04 02:52:15 +0300

    Before merging with george

--- legal/sdk/core/src/commonMain/kotlin/identifier/transformers/CorporateOutputTransformers.kt
+++ legal/sdk/core/src/commonMain/kotlin/identifier/transformers/CorporateOutputTransformers.kt
@@ -2,19 +2,22 @@ package identifier.transformers
 
 import identifier.fields.CorporateOutput
 import identifier.params.CorporateParams
+import kase.catching
 
-fun CorporateOutput.toParams() = CorporateParams(
-    name = name ?: throw IllegalArgumentException("Name must not be null"),
-    contactName = contactName,
-    contactEmail = contactEmail,
-    contactPhone = contactPhone?.toString(),
-    contactRole = contactRole,
-    industry = industry,
-    registrationNo = registrationNo,
-    registrationDate = registrationDate,
-    tin = tin,
-    vat = vat,
-    website = website,
-    hqLocation = headquarters,
-    businessType = businessType,
-)
+fun CorporateOutput.toParams() = catching {
+    CorporateParams(
+        name = name ?: throw IllegalArgumentException("Name must not be null"),
+        contactName = contactName,
+        contactEmail = contactEmail,
+        contactPhone = contactPhone?.toString(),
+        contactRole = contactRole,
+        industry = industry,
+        registrationNo = registrationNo,
+        registrationDate = registrationDate,
+        tin = tin,
+        vat = vat,
+        website = website,
+        hqLocation = headquarters,
+        businessType = businessType,
+    )
+}
--- legal/sdk/core/src/commonMain/kotlin/identifier/transformers/IndividualOutputTransformers.kt
+++ legal/sdk/core/src/commonMain/kotlin/identifier/transformers/IndividualOutputTransformers.kt
@@ -2,15 +2,18 @@ package identifier.transformers
 
 import identifier.fields.IndividualOutput
 import identifier.params.IndividualParams
+import kase.catching
 
-fun IndividualOutput.toParams() = IndividualParams(
-    name = name ?: throw IllegalArgumentException("Name must not be null"),
-    email = email,
-    phone = phone?.toString(),
-    title = title,
-    dob = dob,
-    gender = gender,
-    idDocumentNumber = idNumber,
-    idDocumentType = idType,
-    location = location,
-)
+fun IndividualOutput.toParams() = catching {
+    IndividualParams(
+        name = name ?: throw IllegalArgumentException("Name must not be null"),
+        email = email,
+        phone = phone?.toString(),
+        title = title,
+        dob = dob,
+        gender = gender,
+        idDocumentNumber = idNumber,
+        idDocumentType = idType,
+        location = location,
+    )
+}
